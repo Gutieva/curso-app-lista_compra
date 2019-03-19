6e7860df3a817a5afd994bb19b548ad5d36686ba
gutieva <evagutierrez2106@gmail.com>
2019-03-19 21:16:25 +0100

    Creados el index.html y main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// (1) Definición de variables
+const articulo = document.getElementById("articulo");
+const cantidad = document.querySelector("#cantidad");
+const prioridad = document.querySelector("#priodidad");
+const agregar = document.querySelector("#agregar");
+
+// (2) Definición de funciones
+
+// (3) Definición de eventos
